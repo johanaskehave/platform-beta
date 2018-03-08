4f2d5d1d6a0a066d5222ab693f7d42ef4e2c21d7
Johan Askehave <jas@outlookenergyservices.com>
2018-03-08 08:24:38 +0100

    Moves models to functions folder

--- server.R
+++ server.R
@@ -2,21 +2,9 @@ library(shiny)
 library(shinydashboard)
 library(plotly)
 
-model_2025 <- function(frokostpause, vakst, elasticitet, ledighed, holdbarhed){
-  round(frokostpause * (vakst * 0.5 + elasticitet * 0.1 + ledighed * 0.25 + holdbarhed * 1.5) * 9876.543,0)
-}
-
-model_2060 <- function(frokostpause, vakst, elasticitet, ledighed, holdbarhed){
-  round(round(frokostpause * (vakst * 0.5 + elasticitet * 0.1 + ledighed * 0.25 + holdbarhed * 1.5) * 11379.5 - 50, digits = -2),0)
-}
-
-#model_2025(frokostpause = 2, vakst = 1.7, elasticitet = -5, ledighed = 3.1, holdbarhed = 0.6)
-#model_2060(frokostpause = 2.3, vakst = 1.7, elasticitet = -5, ledighed = 3.1, holdbarhed = 0.6)
+source(list.files("functions", full.names = TRUE))
 
 server <- function(session, input, output) {
-  
-  set.seed(122)
-  histdata <- rnorm(500)
   
   output$plot1 <- renderPlotly({
     
