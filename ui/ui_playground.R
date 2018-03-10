358803b963525e1c723f144a1b9fcf3ab84af2b9
Mohammad Mahroz <m.mahroz.52@gmail.com>
2018-03-10 05:01:58 +0500

    Added to DISQUS
    Code to add diqus comment section

--- ui/ui_playground.R
+++ ui/ui_playground.R
@@ -26,3 +26,21 @@ fluidRow(
          )
   )
 )
+
+fluidRow(
+  div(id="disqus_thread",
+      HTML(
+        "<script>
+    (function() {  
+        var d = document, s = d.createElement('script');
+        
+        s.src = '//https://technovelty-com.disqus.com/embed.js';
+        
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href='https://disqus.com/?ref_noscript' rel='nofollow'>comments powered by Disqus.</a></noscript>"
+      )
+  )
+)
