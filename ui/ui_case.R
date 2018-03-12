24929f8f25ede8ad82ab3e211220bbdb01761298
Johan Askehave <jas@outlookenergyservices.com>
2018-03-12 21:44:27 +0100

    Alters ui case

--- ui/ui_case.R
+++ ui/ui_case.R
@@ -5,13 +5,13 @@ wellPanel(
   h1(class="siteTitle","Case beskrivelse"),
           
   div(class="main-wrap",
-    h3(class="animated bounceInDown","Hvordan kan beskæftigelsen påvirkes ved at øge de offentlige ansattes arbejdstid?"),
+    h3(class="animated bounceInDown","Hvordan påvirkes beskæftigelsen ved at øge de offentlige ansattes arbejdstid?"),
     
-    h4(class="animated bounceInDown","Dette spørgsmål er relevant at undersøge som baggrund for de overenskomstforhandlinger, som skal genforhandles 1. april 2018. Analysen vil være et centralt element i debatten, hvorfor det er essentielt, at samtlige beregninger og forudsætninger er tilgængelige for offentligheden."), 
+    h4(class="animated bounceInDown","Dette spørgsmål er relevant at undersøge som baggrund for de overenskomstforhandlinger, der skal genforhandles 1. april 2018. Analysen vil være et centralt element i debatten, hvorfor det er essentielt, at samtlige beregninger og forudsætninger er tilgængelige for offentligheden."), 
     
-    h4(class="animated bounceInDown","DA har anmodet Finansministeriets om at udarbejde analysen. Det har naturligvis været et krav fra DAs side, at data, forudsætninger og algoritmer, som ligger til grund for analysen, er tilgængelige for offentligheden."),
+    h4(class="animated bounceInDown","Dansk Arbejdsgiverforening har anmodet Finansministeriets om at udarbejde analysen. Det har naturligvis været et krav fra Dansk Arbejdsgiverforenings side, at data, forudsætninger og algoritmer, som ligger til grund for analysen, gøres tilgængeligt for offentligheden."),
     
-    h4(class="animated bounceInDown","Det er også vigtigt for DA, at interesserede borgere kan arbejde videre med Finansministeriets model samt fortage følsomhedsanalyser på deres resultater. DA anvender derfor platform-beta."),
+    h4(class="animated bounceInDown","Det er også vigtigt for Dansk Arbejdsgiverforening, at interesserede borgere kan arbejde videre med Finansministeriets model samt fortage følsomhedsanalyser på deres resultater. Dansk Arbejdsgiverforening anvender derfor platform-beta."),
     
     h4(class="animated bounceInDown","Link to rå data: www.eksempel.com"),
     h4(class="animated bounceInDown","Link til model: www.github.com/eksempel"),
