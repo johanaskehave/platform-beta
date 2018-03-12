350fb2e674da4a994cae6def529128b926025a16
Johan Askehave <jas@outlookenergyservices.com>
2018-03-12 14:45:01 +0100

    Adds links to about

--- ui/ui_about.R
+++ ui/ui_about.R
@@ -35,14 +35,10 @@ wellPanel(
   ),
   div(class="gap-div"),
   div(class="main-wrap",
-      h3(class="animated bounceInLeft","Hvordan kan beskæftigelsen påvirkes ved at øge de offentlige ansattes arbejdstid?"),
+      h3(class="animated bounceInLeft","Hvad får I ud af det?"),
       
-      h4(class="animated bounceInRight","Dette spørgsmål er relevant at undersøge som baggrund for de overenskomstforhandlinger, som skal genforhandles 1. april 2018. Analysen vil være et centralt element i debatten, hvorfor det er essentielt, at samtlige beregninger og forudsætninger er tilgængelige for offentligheden."), 
-      
-      h4(class="animated bounceInRight","DA har anmodet Finansministeriets om at udarbejde analysen. Det har naturligvis været et krav fra DAs side, at data, forudsætninger og algoritmer, som ligger til grund for analysen, er tilgængelige for offentligheden."),
-      
-      h4(class="animated bounceInRight","Det er også vigtigt for DA, at interesserede borgere kan arbejde videre med Finansministeriets model samt fortage følsomhedsanalyser på deres resultater. DA anvender derfor platform-beta.")
-      
+      h4(class="animated bounceInRight","Platformen er en enormt omkostningseffektiv måde at få borgeren inddraget på en struktureret og faglig måde. Det vil skabe transparens i databehandling og mulighed for innovativ idéudvikling. Ydermere vil jeres organisation på en struktureret og overskuelig måde, få indsigt i resultatet af analyser lavet på deres data, idet resultater, feedback, diskussioner baseret på et datagrundlag vil være tilgængeligt på platformen og på GitHub.")
+
       
   )
 )
