b6b7c0ef0fa98f07d1df9570bc318f7bf0844f5c
Johan Askehave <jas@outlookenergyservices.com>
2018-03-12 21:44:16 +0100

    Alters ui about

--- ui/ui_about.R
+++ ui/ui_about.R
@@ -6,16 +6,16 @@ wellPanel(
   div(class="main-wrap",
       h3(class="animated bounceInLeft","Hvad er det?"),
       
-      h4(class="animated bounceInRight","Denne platform assisterer jeres organisation i processen med at forvandle rå data til innovative analyser, idéer og løsninger. Med denne platform får I maksimalt udbytte af den indsats, der i forvejen bliver lagt i dataindsamling, -udformning og ikke mindst udlægning på offentlige åbne dataplatforme. ")
+      h4(class="animated bounceInRight","Denne platform assisterer jeres organisation i processen med at forvandle rå data til innovative analyser, idéer og løsninger. Med denne platform får I maksimalt udbytte af den indsats, der i forvejen bliver lagt i dataindsamling, -udformning og ikke mindst udlægning på offentlige åbne dataplatforme.")
       
   ),
   div(class="gap-div"),
   div(class="main-wrap",
       h3(class="animated bounceInLeft","Hvorfor?"),
       
-      h4(class="animated bounceInRight","I denne demo har vi valgt at bruge DREAM-modellen, som er udviklet af finansministeriet for Dansk Arbejdsgiverforening, til at forudsige hvordan beskæftigelsen påvirkes i henholdsvis den offentlige og den private sektor, at de offentlige ansattes arbejdstid øges."), 
+      h4(class="animated bounceInRight","I denne demo tager vi udgangspunkt i den analyser, som finansministeriet har udviklet for Dansk Arbejdsgiverforening med henblik på at forudsige hvordan beskæftigelsen påvirkes i henholdsvis den offentlige og den private sektor, at de offentlige ansattes arbejdstid øges. Finansministeriet har baseret analysen på deres DREAM model."), 
       
-      h4(class="animated bounceInRight","DREAM-modellen ligger ikke tilgængeligt for offentligheden, men det gør en rapporten med hovedresultatet, der lister de mange antagelser of forudsætninger, der ligger bag DREAM-modellen. Rapporten er svær at gennemskue og mange beregninger er baseret på ”egne tal”. Det gør det svært for borgere, politikere, journalister mv. at gennemskue hvad tallene, som modellen forudsiger egentlig siger noget om."),
+      h4(class="animated bounceInRight","DREAM-modellen er ikke tilgængeligt for offentligheden, men det er rapporten med hovedresultatet, der lister de mange antagelser of forudsætninger, der ligger bag DREAM-modellen. Rapporten er svær at gennemskue og mange resultater er baseret på ”egne beregninger”. Det gør det svært for borgere, politikere, journalister mv. at gennemskue hvad tallene, som modellen forudsiger egentlig siger noget om."),
       
       h4(class="animated bounceInRight","Ved at offentliggøre DREAM-modellen, vil alle efter evne kunne tilgå den og dermed forstå og overveje antagelser og forudsætninger og medtage disse i meningsdannelser og diskussioner om emnet.")
       
@@ -25,9 +25,9 @@ wellPanel(
   div(class="main-wrap",
       h3(class="animated bounceInLeft","Hvordan fungerer det?"),
       
-      h4(class="animated bounceInRight","I vil kunne udlægge en case, en problemstilling eller en undren, typisk i forbindelse med en større eller mindre mængde data. I den anden ende af platformen vil der være et community af data scientists; studerende og interesserede borgere der har kapaciteterne og evnerne til at analysere, udvikle modeller og visualisere dataene med fokus på casen, problemet eller den undren, der er blevet fremlagt."), 
+      h4(class="animated bounceInRight","I vil kunne udlægge en case, en problemstilling eller en undren, typisk i forbindelse med en større eller mindre mængde data. I den anden ende af platformen vil der være et community af data scientists, studerende og interesserede borgere, der har kapaciteterne og evnerne til at analysere, udvikle modeller og visualisere dataene med fokus på casen, problemet eller den undren, der er blevet fremlagt."), 
       
-      h4(class="animated bounceInRight","Analyserne, modellerne og visualiseringerne vil blive præsenteret på platformen via en Playground med et link til GitHub, hvor den fulde rapport inkl. koden der er blevet lavet, vil ligge. Herefter kan rapporten eller koden blive videreudviklet på af andre brugere, indgå i en læringskontekst mellem brugerne eller skabe grundlag for en debat om selve emnet."),
+      h4(class="animated bounceInRight","Analyserne, modellerne og visualiseringerne vil blive præsenteret på platformen via en Playground med et link til GitHub, hvor den fulde rapport inkl. koden der er blevet lavet, vil ligge. Herefter kan rapporten eller koden blive videreudviklet af andre brugere, indgå i en læringskontekst mellem brugerne eller skabe grundlag for en debat om selve emnet."),
       
       h4(class="animated bounceInRight","Platformen er en enormt omkostningseffektiv måde at få borgeren inddraget på en struktureret og faglig måde. Det vil skabe transparens i databehandling og mulighed for innovativ idéudvikling. Ydermere vil jeres organisation på en struktureret og overskuelig måde, få indsigt i resultatet af analyser lavet på deres data, idet resultater, feedback, diskussioner baseret på et datagrundlag vil være tilgængeligt på platformen og på GitHub.")
       
@@ -37,7 +37,7 @@ wellPanel(
   div(class="main-wrap",
       h3(class="animated bounceInLeft","Hvad får I ud af det?"),
       
-      h4(class="animated bounceInRight","Platformen er en enormt omkostningseffektiv måde at få borgeren inddraget på en struktureret og faglig måde. Det vil skabe transparens i databehandling og mulighed for innovativ idéudvikling. Ydermere vil jeres organisation på en struktureret og overskuelig måde, få indsigt i resultatet af analyser lavet på deres data, idet resultater, feedback, diskussioner baseret på et datagrundlag vil være tilgængeligt på platformen og på GitHub.")
+      h4(class="animated bounceInRight","Platformen er en enormt omkostningseffektiv måde at få borgeren inddraget på en struktureret og faglig måde. Det vil skabe transparens materiale, der ligger til grund for mange debatter og ikke mindst skabe mulighed for innovativ idéudvikling. Ydermere vil jeres organisation på en struktureret og overskuelig måde, få indsigt i resultatet af analyser lavet på deres data, idet resultater, feedback, diskussioner baseret på datagrundlaget vil være tilgængeligt på platformen og på GitHub.")
 
       
   )
